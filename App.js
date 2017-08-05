20b5002f82c958a4d40e3595c727d11e2293a6b1
Kevin Hammond <klhammond@sep.com>
2017-08-05 12:11:56 -0400

    Split out home screen.

--- App.js
+++ App.js
@@ -2,47 +2,22 @@ import React from 'react';
 import { StyleSheet, Platform, StatusBar, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { BaconMethodScreen } from './BaconMethodScreen'
-
-class HomeScreen extends React.Component {
-    static navigationOptions = {
-      title: "Bakin' Bacon",
-      headerLeft: <Button onPress={() => props.navigation.navigate('DrawerOpen')} title= "🥓" />
-    };
-
-  render() {
-    return (
-      <Button
-        title="?"
-        onPress={() => this.props.navigation.navigate('BaconMethodScreen')}
-      />
-    );
-  }
-}
+import { HomeScreen } from './HomeScreen'
 
 const AppStack = StackNavigator(
     {
-        Home: {
-            screen: HomeScreen
-        },
         BaconMethodScreen: {
             screen: BaconMethodScreen
+        },
+        Home: {
+            screen: HomeScreen
         },
     },
     {
         cardStyle: {
-          paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+            paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
         }
-      }
-  );
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    }
+);
 
 export default AppStack;
---
+++ HomeScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+import * as Colors from './Colors';
+
+export class HomeScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: 'center', color: Colors.primary }}>Home Screen</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.backgroundColor,
+  },
+});
