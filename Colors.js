badaf07aacb7b51c7fddbf1e2eecad0152e8582e
Kevin Hammond <klhammond@sep.com>
2017-08-05 11:54:28 -0400

    Added Bacon colors

---
+++ Colors.js
@@ -0,0 +1,5 @@
+export const primary = '#B41F1F';
+export const secondary = '#ddd';
+export const primaryDark = '#9f1313';
+export const backgroundColor = '#eebf77';
+export const titleColor = '#ffffff';
