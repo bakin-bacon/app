cc52514167066437d3f787d1d2e1a0c1a0f0fb49
Grant Kimsey <gwkimsey@sep.com>
2017-08-05 23:34:09 -0400

    Get feedback button highlighting working

--- BaconFeedbackScreen.js
+++ BaconFeedbackScreen.js
@@ -15,11 +15,18 @@ export class BaconFeedbackScreen extends React.Component {
   constructor(){
     super();
     this.api = new BakinBaconApi();
+    this.state = {
+      feedback: null
+    };
     //this.api.getBaconBits(() => {});
   }
 
   submitFeedback() {
-    this.api.postBaconBit({duration: 1200, timestamp: new Date().toISOString(), bsi: this.bsi}, () => {});
+    if(this.bsi != null){
+      console.log(this.bsi, 'feedback submitted')
+      this.api.postBaconBit({duration: 1200, timestamp: new Date().toISOString(), bsi: this.bsi}, () => {});
+    }
+
     //TODO: Make modal and close window
   }
 
@@ -28,33 +35,41 @@ export class BaconFeedbackScreen extends React.Component {
     this.props.navigation.setParams({ handleSubmit: submitFeedback });
   }
 
-  pressedTooCrispy(){
+  async pressedTooCrispy(){
     this.bsi = -1;
-    //TODO: Set border color
+    await this.setState({feedback: 'crispy'});
   }
 
-  pressedPerfect(){
+  async pressedPerfect(){
     this.bsi = 0;
+    await this.setState({feedback: 'perfect'});
   }
 
-  pressedTooFloppy(){
+  async pressedTooFloppy(){
     this.bsi = 1;
+    await this.setState({feedback: 'floppy'});
   }
 
-  render(){
-    var imageDiameter = Dimensions.get('window').height / 5.5;
-
-    var baconImageStyle = {
+  getBaconImageStyle(imageDiameter, borderColor){
+    return {
       width: imageDiameter,
       height: imageDiameter,
       borderRadius: imageDiameter / 2,
-      borderColor: Colors.primaryDark,
-      borderWidth: 2,
-      marginTop: 25 };
+      borderColor: borderColor,
+      borderWidth: 3,
+      marginTop: 40 };
+    }
+
+  render(){
+    var imageDiameter = Dimensions.get('window').height / 6;
 
     const onPressedCrispy = this.pressedTooCrispy.bind(this);
     const onPressedPerfect = this.pressedPerfect.bind(this);
     const onPressedFloppy = this.pressedTooFloppy.bind(this);
+
+    var crispyColor = this.state.feedback == 'crispy' ? Colors.primaryDark : Colors.secondary;
+    var perfectColor = this.state.feedback == 'perfect' ? Colors.primaryDark : Colors.secondary;
+    var undercookedColor = this.state.feedback == 'floppy' ? Colors.primaryDark : Colors.secondary;
 
     return(
       <View style={styles.masterContainer}>
@@ -62,21 +77,21 @@ export class BaconFeedbackScreen extends React.Component {
           <TouchableOpacity onPress={onPressedCrispy}>
             <Image
               source={require('./images/Burnt-Bacon.png')}
-              style={baconImageStyle}/>
+              style={this.getBaconImageStyle(imageDiameter, crispyColor)}/>
           </TouchableOpacity>
           <Text style={styles.baconLabel}>Too Crispy</Text>
 
           <TouchableOpacity onPress={onPressedPerfect}>
             <Image
               source={require('./images/JustRight-Bacon.png')}
-              style={baconImageStyle}/>
+              style={this.getBaconImageStyle(imageDiameter, perfectColor)}/>
           </TouchableOpacity>
           <Text style={styles.baconLabel}>Just Right!</Text>
 
           <TouchableOpacity onPress={onPressedFloppy}>
             <Image
               source={require('./images/Undercooked-Bacon.png')}
-              style={baconImageStyle}/>
+              style={this.getBaconImageStyle(imageDiameter, undercookedColor)}/>
           </TouchableOpacity>
           <Text style={styles.baconLabel}>Too Floppy</Text>
         </View>
